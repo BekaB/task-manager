8a968d3d032e6c2641ea10b4bb20342552962afd
Bereket Negash <negash.bereket.se@gmail.com>
2024-12-06 04:35:32 +0400

    authorization with middleware

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -26,55 +27,43 @@ router.post('/users', async (req, res) => {
  router.post('/users/login', async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        // console.error(e) // Add logging for debug
         const token = await user.generatAuthToken()
-        res.send({user,token})
+        res.send({user, token})
     }catch(e){
-        console.error(e) // Add logging for debug
+        //console.error(e) // Add logging for debug
         res.status(400).send()
     }
  })
 
-router.get('/users', async (req, res) => {
-
+ router.post('/users/logout', auth, async (req, res) => {
     try{
-        const user = await User.find({})
-        res.send(user)
-    }catch(e){
-        res.status(500).send(e)
-    }
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
 
-    // User.find({}).then((users) => {
-    //     res.send(users)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try{
-        const user = await User.findById(_id)
-        if(!user){
-            console.log(req.params.id)
-            return res.status(404).send()
-        }
-        res.send(user)
+        res.send()
     }catch(e){
         res.status(500).send()
+
     }
+ })
 
-    // User.findById(_id).then((user) => {
-    //     if(!user){
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
+ router.post('/users/logoutAll', auth, async(req, res) =>{
+    try{
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    }catch(e){
+        req.status(200).send()
+    }
+ })
 
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
+router.get('/users/me', auth, async (req, res) => {
+
+    res.send(req.user)
 })
+
 
 router.patch('/users/:id', async(req, res) => {
     const updates = Object.keys(req.body)
