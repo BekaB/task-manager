2874fe9aac29978979571f1b0d7ede6507049039
Bereket Negash <negash.bereket.se@gmail.com>
2024-12-07 05:33:15 +0400

    documents relation

--- src/models/task.js
+++ src/models/task.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
 
-const taskSchema = mongoose.Schema({
+const Task = mongoose.model('Task', {
     description: {
         type: String,
-        require: true,
+        required: true,
         trim: true
     },
     completed: {
@@ -13,15 +12,37 @@ const taskSchema = mongoose.Schema({
     },
     owner: {
         type: mongoose.Schema.Types.ObjectId,
-        require: ture
+        required: true,
+        ref: 'User'
     }
 })
 
-taskSchema.pre('task', async function() {
-    const task = this
+module.exports = Task
 
-})
+// const mongoose = require('mongoose')
+// const bcrypt = require('bcrypt')
 
-const task = new mongoose.model('tasks', taskSchema)
+// const taskSchema = mongoose.Schema({
+//     description: {
+//         type: String,
+//         require: true,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     owner: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         require: true
+//     }
+// })
 
-module.exports = task
+// taskSchema.pre('task', async function() {
+//     const task = this
+
+// })
+
+// const task = new mongoose.model('tasks', taskSchema)
+
+// module.exports = task
