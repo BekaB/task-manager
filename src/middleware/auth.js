b277c2e1eb92343e36fae0373a48ad46e518dace
Bereket Negash <negash.bereket.se@gmail.com>
2024-12-26 04:10:23 +0400

    email sending and file upload

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try{
         const token = req.header('Authorization').replace('Bearer ','')
-        const decoded = jwt.verify(token , 'thisismynewcourse')
+        const decoded = jwt.verify(token , process.env.JWT_SECRET)
         const user = await User.findOne({_id: decoded._id , 'tokens.token': token})
 
         if(!user){
@@ -14,7 +14,6 @@ const auth = async (req, res, next) => {
         req.token = token
         req.user = user
         next()
-
     }catch(e){
         //console.error(e) // Add logging for debug
         res.status(401).send({error: 'please authenticat'})
